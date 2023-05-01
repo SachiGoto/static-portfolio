94f34f613e20b931184ed72751e6cfd7f315831a
Sachi Goto <70562492+SachiGoto@users.noreply.github.com>
2023-04-30 17:15:36 -0700

    added my new prject

--- src/app/project-details/project-details.component.ts
+++ src/app/project-details/project-details.component.ts
@@ -10,16 +10,6 @@ import { HttpClient } from '@angular/common/http'
 import { Meta } from '@angular/platform-browser';
 import { Title } from '@angular/platform-browser';
 
-
-
-// import AOS from 'aos';
-
-
-
-
-
-
-
 @Component({
   selector: 'app-project-details',
   templateUrl: './project-details.component.html',
@@ -27,11 +17,7 @@ import { Title } from '@angular/platform-browser';
 })
 
 
-
-
 export class ProjectDetailsComponent implements OnInit {
-
-
   // private observer:IntersectionObserver;
 
   constructor(private route:ActivatedRoute, private hp:HttpClient, private http:CommonService, private titleService:Title,private meta:Meta) {
@@ -47,18 +33,9 @@ export class ProjectDetailsComponent implements OnInit {
       {name:"title" , content:"Sachi Goto - Project Details"}
     )
   }
-  // project:ProjectDetail;
-//  project:ProjectDetail;
-
  project:any;
 
-
-
-
-
 server = environment.server;
-
-// websiteHeroImage='';
 
 scrollup(){
   window.scroll({
@@ -84,64 +61,38 @@ scrollTo(summary:any){
 
   ngOnInit(): void {
 
-  //  console.log("project json " , this.http.getJson);
-
-  //  this.projectJson = this.http.getJson()
-
-
-
-  //  this.http.getAboutMe().subscribe(res=>{
-
-  //  })
-  //  this.projectJson = this.http.getJson
-
-
-
     window.scrollTo(0,0);
-    // AOS.init()
     let id:any= this.route.snapshot.paramMap.get("projectID");
-    let slug:any = this.route.snapshot.paramMap.get("slug")
-//     this.http.getProjectById(id).subscribe(project=>{
-// this.project = project.data;
+ 
+    let slug:any = this.route.snapshot.paramMap.get("slug");
 
-//     })
 this.hp.get('assets/projects.json').subscribe((res)=>{
-  this.project = res
-  this.project = this.project.data[id-1];
-  console.log(this.project.data[id-1]);
-  console.log("category is ", this.project.data.attributes.Category)
+  this.project = res;
+  console.log(this.project.data)
+  for(let i=0; i<this.project.data.length; i++){
+    console.log('id ', id, this.project.data[i].id)
+    console.log(this.project.data[i].id == id)
+    if(this.project.data[i].id == id){
+      this.project = this.project.data[i]
+    }
+      // console.log('hello')
+  }
+  // for(const item in this.project.data.attributes){
+  //        console.log('item is ' , item)
+  // }
+  // if(this.project.data.id === id){
+  //   this.project = this.project.data.attributes
+  // }
+  
+
+  // console.log(this.project.data[id-1]);
+  // console.log("category is ", this.project.data.attributes.Category)
+
 
  })
 
 
   Aos.init();
-
-// gsap.registerPlugin(ScrollTrigger);
-
-  // this.http.getAboutMe().subscribe(res=>{
-  //   console.log(res);
-  //   // better make an interface so that I can see what is in the data.
-  //   this.websiteHeroImage = environment.server + res.data.attributes.WebsiteHeroImage.data.attributes.url;
-
-  //   console.log("webstieImage" , this.websiteHeroImage);
-
-
-
-  // })
-
-
-
-
-  //   const elementToTrack = document.getElementById("test")
-
-  //   const observer = new IntersectionObserver((entries)=>{
-  //     entries.forEach(entry => {
-  //       console.log("Intersecting : " , entry.isIntersecting)
-  //   })
-  // })
-
-
-
 
 
 }
@@ -152,12 +103,12 @@ selected = false;
 
 ngAfterViewChecked(){
   const mobileScreen = window.matchMedia("(min-width: 768px)");
-  console.log(mobileScreen.matches)
+  // console.log(mobileScreen.matches)
   const container = document.querySelector(".imagecontainer")
   const titleContainer = document.querySelector(".devtitleContainer")
   if(mobileScreen.matches === true){
     if(!this.selected && container){
-      console.log(container, titleContainer)
+      // console.log(container, titleContainer)
       this.selected = true
       const scrollBox = gsap.timeline({
         scrollTrigger: {
@@ -180,68 +131,4 @@ ngAfterViewChecked(){
 }
 
 }
-
-// skillContainer(){
-//   // console.log('works')
-//   const titleContainer = document.querySelector(".developmentContainer")
-//   // console.log(titleContainer)
-//   const imageContinaer = document.querySelector(".imagecontainer")
-// console.log(titleContainer)
-//     const scrollBox = gsap.timeline({
-//       scrollTrigger: {
-//         trigger: imageContinaer,
-//         pin: false,
-//         start: '2000px center',
-//         end: '2500px bottom',
-//         markers: true,
-//         toggleActions: 'play none none reverse',
-//       },
-//     });
-//     scrollBox.from(titleContainer, { opacity: 0, duration:2});
-
-// }
-
-
-
-//       //  let websitepages = angular.element(document.querySelectorAll(".image"));
-//    let websitepages = document.querySelectorAll(".image");
-//    console.log(websitepages);
-//       const options = {
-//         root:null, // it is the viewport
-//         threshold:0,  // 0 to 1 scale. if it's 1, 100 percent of the content needs to be in viewport to fire the function. 0.25 means 25% of specified content.
-
-//         rootMargin: "-200px", // pull margin to certain amount to change the firing point. if it's 150px, 150px below the top page will be the trigger point. It has to be either % or px.
-
-//       };
-
-//        // every time, the specified section is intersected (touch the top of the page), function gets triggered.
-//        const observer = new IntersectionObserver(function(entries, oberver){
-//         entries.forEach(entry=>{
-//           console.log(entry);
-//           if(entry.isIntersecting)
-//           entry.target.classList.add('show');
-
-//           else{
-//             entry.target.classList.remove('show');
-//           }
-
-//           // observer.unobserve(entry.target);  // observe only once.  - useful for lazy loading
-//         })
-//       }, options);
-
-//       websitepages.forEach(pages=>{
-//       observer.observe(pages);
-//     })
-
-
-//     // observer.observe(websitepages);
-
-
-
-
-
-
-
-
-  // }
 
